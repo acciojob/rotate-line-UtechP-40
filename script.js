ef990d5dc16bdf66ddec596694a5838a99769ac8
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-27 23:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
 let line = document.querySelector("#line");
 let angle = 0;
 
 setInterval(() => {
     angle += 2; 
-    line.style.transform = `rotate(${angle}deg)`;
+    line.style.rotate = `${angle}deg`;
 }, 20);
